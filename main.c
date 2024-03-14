be3d9b5d68d67731093bd699353fae26ffd48972
Amr Salhin <amr.a.salhin@gmail.com>
2024-03-14 13:03:00 +0200

    add tabs

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 void sort(int arr[]);
 int main(){
 
-int x =10;
-int g =70;
-char temp[] = "test branching";
-int new;   
-int old;
+	int x =10;
+	int g =70;
+	char temp[] = "test branching";
+	int new;   
+	int old;
 }
 Y}W0Qcu/qn
